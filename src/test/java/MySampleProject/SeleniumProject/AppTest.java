691fc97a04533dfe4f7ea817e460480e091e6d00
Abhishek Redij <abhishekr@citiustech.com>
2023-07-26 13:06:01 +0530

    browser actions added

--- src/test/java/MySampleProject/SeleniumProject/AppTest.java
+++ src/test/java/MySampleProject/SeleniumProject/AppTest.java
@@ -13,22 +13,15 @@ public class AppTest
 {
     /**
      * Rigorous Test :-)
+     * @throws InterruptedException 
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue() throws InterruptedException
     {
     	System.out.println( "Hello World!" );
-        //System.out.println(System.getProperty("user.dir"));
-        //System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/drivers/chromedriver.exe");
-        
-        //ChromeDriverService service = new ChromeDriverService.Builder().build();
-        //service.setExecutable(System.getProperty("user.dir") + "/src/main/resources/drivers/chromedriver.exe");
-        
-        //driver = new ChromeDriver(service);
         WebDriver driver = new ChromeDriver();
         driver.get("https://demoqa.com/buttons");
-        //driver.wait(5000);
-        //driver.close();
-    	assertTrue( true );
+        assertTrue( true );
+        driver.close();
     }
 }
